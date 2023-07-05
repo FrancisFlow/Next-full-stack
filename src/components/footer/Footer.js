a99e187178b56245e7572d6d42b55db43664eacf
francisflow <Iamngigikaranja1@aol.com>
2023-07-06 02:45:35 +0300

    Added and aligned footer icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,49 @@
+import Image from "next/image";
+import styles from "./footer.module.css";
+
+const imgurls= [
+    {
+        id:1,
+        src:"/images/1.png"
+    },
+
+    {
+        id:2,
+        src:"/images/2.png"
+
+    },
+    {
+        id:3,
+        src:"/images/3.png"
+
+    },
+
+]
+
 const Footer = () => {
     return ( 
         <>
-        <div className="copyright">
-        <h2 className="bg-blue-400" >Ich bin der footer</h2>
+        <div className={styles.container}>
+        <div>
+            @2023 Lamamia. All rights reserved.
 
         </div>
-        <div className="brandlinks">
+        
+        <div className={styles.brandlinks}>
+          
+            { 
+            imgurls.map(imgurl =>(
+                
+
+                    <Image key={imgurl.id} className={styles.social} src={imgurl.src} height={15}
+                    width={15} alt="image brand"  />
+                ))
+            }
+           
+          
+            
+        </div>
+       
 
         </div>
         </>
