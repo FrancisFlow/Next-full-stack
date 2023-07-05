89883319aa62491955d1cee721128e218e7d9dc5
francisflow <Iamngigikaranja1@aol.com>
2023-07-06 00:53:03 +0300

    Created css module files for each page

---
+++ src/app/contact/loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return ( 
+        <h2 className="text-2xl text-gray-400 bg-green-400" >
+            Loading ...
+        </h2>
+     );
+}
+ 
+export default Loading;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return ( 
+        <>
+        <div className="copyright">
+        <h2 className="bg-blue-400" >Ich bin der footer</h2>
+
+        </div>
+        <div className="brandlinks">
+
+        </div>
+        </>
+     );
+}
+ 
+export default Footer;
