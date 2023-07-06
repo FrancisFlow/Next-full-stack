debfe1d02855f5f324b8a49deca137987a211904
francisflow <Iamngigikaranja1@aol.com>
2023-07-06 22:54:20 +0300

    Created module for homepage and styled text and button

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from "./page.module.css";
+import Hero from "public/images/hero.png";
 
 export default function Home() {
   return (
     <>
-    <div className="grid grid-flow-col ">
-      <div className="">
-        <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600' >Better design for your digital products.</h1>
-        <h3 className='p-4 text-2xl' >Turning your idea into Reality. We bring together the teams from the global tech industry</h3>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600" >Better design for your digital products.</h1>
+        <h3 className={styles.des} >Turning your idea into Reality. We bring together the teams from the global tech industry</h3>
         <div className="btn">
-        <Link className="bg-green-500 px-1 rounded-sm"  href="/portfolio" >
-          See Our works
-        </Link>
+          <button className={styles.button} >
+                      See Our works
+
+          </button>
+        
           
         </div>
 
       </div>
-      <div className="imageContainer">
-        <Image src={"/images/hero.png"} width={500} height={500} 
+      <div className={styles.item}>
+        <Image className={styles.img} src={Hero} 
       alt='Hero image' />
       </div>
       
