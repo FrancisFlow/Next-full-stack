1bac842a7a3198cf571ca0090065aa14d2b1696e
francisflow <Iamngigikaranja1@aol.com>
2023-07-07 00:31:48 +0300

    Replaced static button with reusable Button component

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,17 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from "./page.module.css";
 import Hero from "public/images/hero.png";
+import Button from '@/components/button/Button';
 
 export default function Home() {
   return (
     <>
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600" >Better design for your digital products.</h1>
+        <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600" >Better design for your digital products.</h1>
         <h3 className={styles.des} >Turning your idea into Reality. We bring together the teams from the global tech industry</h3>
         <div className="btn">
-          <button className={styles.button} >
-                      See Our works
-
-          </button>
+          <Button url={"/portfolio"} text="See our work" />
         
           
         </div>
