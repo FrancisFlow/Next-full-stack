5520377c73026a450a529610f0cb4c0310ad7ffd
francisflow <Iamngigikaranja1@aol.com>
2023-07-06 16:03:33 +0300

    Created graditent text for hero heading

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-    <div className="home">
-      <Image src={"/images/hero.png"} width={500} height={500} 
+    <div className="grid grid-flow-col ">
+      <div className="">
+        <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600' >Better design for your digital products.</h1>
+        <h3 className='p-4 text-2xl' >Turning your idea into Reality. We bring together the teams from the global tech industry</h3>
+        <div className="btn">
+        <Link className="bg-green-500 px-1 rounded-sm"  href="/portfolio" >
+          See Our works
+        </Link>
+          
+        </div>
+
+      </div>
+      <div className="imageContainer">
+        <Image src={"/images/hero.png"} width={500} height={500} 
       alt='Hero image' />
+      </div>
+      
     </div>
     </>
   )
