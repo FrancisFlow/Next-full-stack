a2ac99b7a24f2071233b02fcdeab4bcf3d273a26
francisflow <Iamngigikaranja1@aol.com>
2023-07-07 01:05:22 +0300

    Added about image and heading

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,25 @@
+import Image from "next/image";
+import styles from "./contact.module.css"
 const ContactPage = () => {
     return ( 
         <>
         <div className="contact">
-            <h2>Contact us</h2>
+            <h1 className="text-5xl text-center font-bold" >Let&apos;s Keep in Touch</h1>
+            <div className="" >
+                <div className="">
+                    <Image  className={styles.contactImg} width={500} height={500} src="/images/contact.png" alt="contact hero"/>
+                </div>
+                <div className="">
+                <form className="form">
+                    <h2>Contact us</h2>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" placeholder="your name"/>
+                    </div>
+                </form>
+                </div>
+
+            </div>
         </div>
         </>
      );
