2ff984b2f8874659c9316de4ed4ba25f4febe490
francisflow <Iamngigikaranja1@aol.com>
2023-07-07 17:54:10 +0300

    Used params to get route from portfolio

--- src/app/portfolio/[category]/page.js
+++ src/app/portfolio/[category]/page.js
@@ -1,8 +1,28 @@
-const CategoryPage = () => {
+import styles from './category.module.css';
+import Button from '@/components/button/Button';
+import Image from 'next/image';
+const CategoryPage = ({params}) => {
     return ( 
         <>
-        <div className="category">
-            <h2>Category page</h2>
+        <div className={styles.container}>
+            <h1 className={styles.catTitle} >
+                {params.category}
+            </h1>
+
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title} >
+                        Test
+                    </h1>
+                    <p className={styles.desc} >Desc</p>
+                    <Button text="See More" url="#"/ >
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image fill={true}
+                    src='/images/apps.jpg'
+                    alt={params.category}/>
+                </div>
+            </div>
         </div>
         </>
      );
--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './portfolio.module.css'
 const Portfolio = () => {
     return ( 
@@ -9,20 +10,29 @@ const Portfolio = () => {
 
             
             <div className={styles.works}>
+                
                 <div className={styles.workItem}>
+                    <Link href={"/portfolio/illustrations"}>
+
                     <h2 className={styles.workText}>
                         Illustrations
                     </h2>
+                    </Link>
+
                 </div>
                 <div className={styles.workItem}>
+                    <Link href={"/portfolio/website"}>
                     <h2 className={styles.workText}>
                         Website
                     </h2>
+                    </Link>
                 </div>
                 <div className={styles.workItem}>
+                    <Link href={"/portfolio/applications"}>
                     <h2 className={styles.workText}>
                         Applications
                     </h2>
+                    </Link>
                 </div>
 
             </div>
