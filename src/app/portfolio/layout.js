1e4e9bed0fe23c68a9ce376135816bc4cb25ae91
francisflow <Iamngigikaranja1@aol.com>
2023-07-07 17:52:56 +0300

    Added a fixed title on every rendered page

---
+++ src/app/portfolio/layout.js
@@ -0,0 +1,13 @@
+const Layout = ({children}) => {
+    return (  
+<>
+<div className="width-full" >
+    <h2 className="text-6xl font-bold" >Our Works</h2>
+    {children}
+</div>
+
+</>
+    );
+}
+ 
+export default Layout;
