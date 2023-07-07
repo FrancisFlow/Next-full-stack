7336034faa30789852c40e086d89170ec198d7f5
francisflow <Iamngigikaranja1@aol.com>
2023-07-07 14:51:45 +0300

    Added background images to our works

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,8 +1,31 @@
+import styles from './portfolio.module.css'
 const Portfolio = () => {
     return ( 
         <>
-        <div className="portfolio">
-            <h2>Portfolio</h2>
+        <div className={styles.subheading}>
+                <h2>Choose a gallery</h2>
+            </div>
+        <div className={styles.portfolio}>
+
+            
+            <div className={styles.works}>
+                <div className={styles.workItem}>
+                    <h2 className={styles.workText}>
+                        Illustrations
+                    </h2>
+                </div>
+                <div className={styles.workItem}>
+                    <h2 className={styles.workText}>
+                        Website
+                    </h2>
+                </div>
+                <div className={styles.workItem}>
+                    <h2 className={styles.workText}>
+                        Applications
+                    </h2>
+                </div>
+
+            </div>
         </div>
         </>
      );
